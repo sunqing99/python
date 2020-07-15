c593b7d348c89a6d791b2d2f2b6a950f5e7be448
Qing Sun <sunq99@gmail.com>
2020-07-14 22:14:39 -0700

    added viv

---
+++ viv.py
@@ -0,0 +1,31 @@
+#!/usr/local/bin/python3
+
+from turtle import *
+import sys
+register_shape('viv.gif')
+shape('viv.gif')
+side = 200
+points = int(sys.argv[1])
+if points > 0:
+  turn = 360.0 / points
+  yoff = -side
+else:
+  turn = 180.0 * (1 - 1.0 / points)
+  yoff = 0.0
+  side *= 2
+
+color('red', 'yellow')
+up()
+setpos(-side,yoff)
+home=pos()
+down()
+
+begin_fill()
+while True:
+    forward(side)
+    stamp()
+    left(turn)
+    if distance(home) < 1:
+        break
+end_fill()
+done()
