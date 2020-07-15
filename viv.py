f1d705dabc21558bd6630ebf6fdde14389d2ac63
Qing Sun <sunq99@gmail.com>
2020-07-15 09:45:44 -0700

    improve

--- viv.py
+++ viv.py
@@ -1,26 +1,27 @@
 #!/usr/local/bin/python3
 
 from turtle import *
-import sys
+import sys, math
 register_shape('viv.gif')
 shape('viv.gif')
-side = 200
+radius = 350
 points = int(sys.argv[1])
 if points > 0:
   turn = 360.0 / points
-  yoff = -side
+  yoff = -radius
+  side = 2.0 * radius * math.sin(math.pi / points)
 else:
   turn = 180.0 * (1 - 1.0 / points)
   yoff = 0.0
-  side *= 2
+  side = 2 * radius
 
 color('red', 'yellow')
+speed(8)
 setup(width=1., height=1., startx=None, starty=None)
 up()
-setpos(-side,yoff)
+setpos(-radius,yoff)
 home=pos()
 down()
-
 begin_fill()
 while True:
     forward(side)
